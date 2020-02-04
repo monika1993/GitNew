78bd72b8ddb83b83077ef49d9a6f4b4304a18da7
monika1993 <monika.rajoriya1@gmail.com>
2020-02-04 20:34:22 +0530

    Third Commit closes #1

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Test Git Project");
+        System.out.println("Third Commit With Issue Close #1");
     }
 }

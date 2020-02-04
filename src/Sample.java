a3c6a1417a6d938caa40aba39cd5a34925ca6dca
monika1993 <monika.rajoriya1@gmail.com>
2020-02-04 20:52:34 +0530

    Fifth Commit With Tag 0.0.2

--- src/Sample.java
+++ src/Sample.java
@@ -3,5 +3,6 @@ public class Sample {
         System.out.println("Test Git Project");
         System.out.println("Third Commit With Issue Close #1");
         System.out.println("Fourth Commit With Issue Close #2");
+        System.out.println("Fifth Commit With Tag 0.0.2");
     }
 }

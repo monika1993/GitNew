d142c787fb3ac85b25c576278db59b9534ed333d
monika1993 <monika.rajoriya1@gmail.com>
2020-02-04 20:47:37 +0530

    Fourth Commit closes #2

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Test Git Project");
         System.out.println("Third Commit With Issue Close #1");
+        System.out.println("Fourth Commit With Issue Close #2");
     }
 }
